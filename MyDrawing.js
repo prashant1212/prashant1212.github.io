badfe2bcce5deef7b68ac53943da77e8e05e041a
prashant1212 <prashantmohan880@gmail.com>
2016-08-31 23:22:56 -0400

    fjgehgue

---
+++ MyDrawing.js
@@ -0,0 +1,40 @@
+
+/* main -- here is where execution begins after window load */
+
+function main() {
+
+    // Get the canvas and context
+    var canvas = document.getElementById("viewport"); 
+    var context = canvas.getContext("2d");
+ 
+    // Create the image
+    //drawRandPixels(context);
+      // shows how to draw pixels
+    
+    drawRandPixelsInInputSpheres(context);
+      // shows how to draw pixels and read input file
+      
+    //drawInputSpheresUsingArcs(context);
+      // shows how to read input file, but not how to draw pixels
+}
+
+// draw random pixels
+function drawRandPixels(context) {
+    var c = new Color(1,0,0,0); // the color at the pixel: probably red
+    var w = context.canvas.width;
+    var h = context.canvas.height;
+    var imagedata = context.createImageData(w,h);
+    const PIXEL_DENSITY = 0.10;
+    var numPixels = (w*h)*PIXEL_DENSITY; 
+    
+    // Loop over 1% of the pixels in the image
+    for (var x=0; x<numPixels; x++) {
+        c.change(Math.random()*255,Math.random()*255,
+            Math.random()*255,255); // rand color
+        drawPixel(imagedata,
+            Math.floor(Math.random()*w),
+            Math.floor(Math.random()*h),
+                c);
+    } // end for x
+    context.putImageData(imagedata, 0, 0);
+} // end draw random pixels
