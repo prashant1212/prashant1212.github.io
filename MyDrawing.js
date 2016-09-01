5e1e0add73ba34a3bf3a56712c9cd1e37584f1ec
prashant1212 <prashantmohan880@gmail.com>
2016-08-31 23:29:58 -0400

    color class

--- MyDrawing.js
+++ MyDrawing.js
@@ -1,3 +1,45 @@
+//CLASSES
+// Color constructor
+class Color {
+    constructor(r,g,b,a) {
+        try {
+            if ((typeof(r) !== "number") || (typeof(g) !== "number") || (typeof(b) !== "number") || (typeof(a) !== "number"))
+                throw "color component not a number";
+            else if ((r<0) || (g<0) || (b<0) || (a<0)) 
+                throw "color component less than 0";
+            else if ((r>255) || (g>255) || (b>255) || (a>255)) 
+                throw "color component bigger than 255";
+            else {
+                this.r = r; this.g = g; this.b = b; this.a = a; 
+            }
+        } // end try
+        
+        catch (e) {
+            console.log(e);
+        }
+    } // end Color constructor
+
+        // Color change method
+    change(r,g,b,a) {
+        try {
+            if ((typeof(r) !== "number") || (typeof(g) !== "number") || (typeof(b) !== "number") || (typeof(a) !== "number"))
+                throw "color component not a number";
+            else if ((r<0) || (g<0) || (b<0) || (a<0)) 
+                throw "color component less than 0";
+            else if ((r>255) || (g>255) || (b>255) || (a>255)) 
+                throw "color component bigger than 255";
+            else {
+                this.r = r; this.g = g; this.b = b; this.a = a; 
+            }
+        } // end throw
+        
+        catch (e) {
+            console.log(e);
+        }
+    } // end Color change method
+} // end color class
+
+
 
 /* main -- here is where execution begins after window load */
 
